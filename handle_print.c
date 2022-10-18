40021341d6e22d9725075b58da23f0002c7de2a6
Deboraholaoluwa <111073521+Deboraholaoluwa@users.noreply.github.com>
2022-10-18 22:22:29 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -7,7 +7,7 @@
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
  * @width: get width.
- * @precision: Precision specification
+ * @precision: Precision specifications
  * @size: Size specifier
  * Return: 1 or 2;
  */
