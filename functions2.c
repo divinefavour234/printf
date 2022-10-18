ad6e4100c92cbde46a8e55d1793c8c31dccdb3e6
Deboraholaoluwa <111073521+Deboraholaoluwa@users.noreply.github.com>
2022-10-18 22:15:20 +0100

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -3,7 +3,7 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
- * @types: List a of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
