5d19a87ca636e11be0cbbb08abd999e7fb52c8c8
Deboraholaoluwa <111073521+Deboraholaoluwa@users.noreply.github.com>
2022-10-18 22:18:39 +0100

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -14,7 +14,7 @@
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
-{ /* char is stored at left and paddind at buffer's right */
+{ /* char is stored at left and padding at buffer's right */
 	int i = 0;
 	char padd = ' ';
 
